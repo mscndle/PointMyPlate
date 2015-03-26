0b85572bfcfaa5d54a5a713939059a93e7339561
Trevor Croft <tcroft@mobiquityinc.com>
2015-03-26 19:36:26 +0100

    initial framework

---
+++ app/src/main/java/com/weightwatchers/pointmyplate/MainActivity.java
@@ -0,0 +1,23 @@
+package com.weightwatchers.pointmyplate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+/**
+ * This activity will bootstrap the app then die
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        startActivity(new Intent(this, ))
+    }
+
+}
---
+++ app/src/main/java/com/weightwatchers/pointmyplate/ui/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.weightwatchers.pointmyplate.ui;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by trevor on 3/26/2015.
+ */
+public class BaseActivity extends ActionBarActivity {
+
+
+}
